d220d21327ee68e6b0704fa6534ae90f1db58b9b
mugunthancloudin <mugunthan@cloudinlabs.com>
2024-02-19 19:45:29 +0530

    home page design update

--- src/components/landingpage/home/home.js
+++ src/components/landingpage/home/home.js
@@ -3,33 +3,34 @@ import "./home.css";
 import title from "../../assets/home/Arodnap FONT-01.svg";
 import twitterLogo from "../../assets/home/twitterLogo.svg";
 import telegramLogo from "../../assets/home/telegramLogo.svg";
-import doge from "../../assets/home/bonk.doge.svg";
-import human from "../../assets/home/sbfMobile.png";
+// import doge from "../../assets/home/bonk.doge.svg";
+// import human from "../../assets/home/sbfMobile.png";
 import Topbar from "../header&footer/navbar";
 
 export default function Home() {
-  const buttonText = "Buy now / ";
+
+  // const buttonText = "Buy now / ";
   // Generate a long string of repeated text
-  const repeatedText = buttonText.repeat(100);
+  // const repeatedText = buttonText.repeat(100);
 
   return (
     <>
-      <div className="container-fluid bg-black action1 pb-5  homeContainer">
-        <div className="row">
-          <Topbar />
+      <div className="container-fluid bg-black action1 homeContainer">
+        <div className="row pt-5">
+          <Topbar  className=""/>
         </div>
         <div className="row d-flex justify-content-center position-relative">
-          <div className="col-lg-6 text-center text-white">
-            <div className="justify-content-center  w-100 align-items-center d-flex">
+          <div className="col-lg-12 text-center text-white">
+            <div className="">
               {/* <span className="homeDoller">$</span> */}
               <img src={title} alt="title" className="title" />
             </div>
             <h1 className="fw-bold mt-3">Look Right, Move Left</h1>
             <h5 className="fw-bold mt-2">
-              Created as a culmination of all the memeable events on solana
+              Created as a culmination of all the memeable events on solana 
             </h5>
 
-            <div className="mediablock w-100" style={{ zIndex: 1000 }}>
+            <div className="mediablock " style={{ zIndex: 1000 }}>
               <div className="mediablock-inside">
                 Our Social Media
                 <a
@@ -38,7 +39,7 @@ export default function Home() {
                   rel="noopener noreferrer"
                   className="social-item"
                 >
-                  <img src={twitterLogo} alt="Twitter" className="w-50" />
+                  <img src={twitterLogo} alt="Twitter" className="sIcon" />
                 </a>
                 <a
                   href="https://t.me/analos_coin"
@@ -46,7 +47,7 @@ export default function Home() {
                   rel="noopener noreferrer"
                   className="social-item"
                 >
-                  <img src={telegramLogo} alt="Telegram" className="w-50" />
+                  <img src={telegramLogo} alt="Telegram" className="sIcon" />
                 </a>
               </div>
               
@@ -58,20 +59,20 @@ export default function Home() {
                 </span>
               
             </div>
-
+{/* 
             <img src={doge} alt="Telegram" className=" homeDoge" />
-            <img src={human} alt="human" className=" homeHuman" />
+            <img src={human} alt="human" className=" homeHuman" /> */}
           </div>
         </div>
         {/* <div className="row"> */}
-        <div className="footer-line mt-3">
+        {/* <div className="footer-line mt-3">
           <div className="scroll">
             <div className="RightToLeft">
               {repeatedText}
               {repeatedText}
             </div>
           </div>
-        </div>
+        </div> */}
         {/* </div> */}
       </div>
     </>
